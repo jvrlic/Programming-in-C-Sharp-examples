5cc9b4a0241cb7a6819b9f5af0c871c8f06083a6
jvrlic <jvrlic@gmail.com>
2023-02-19 10:38:32 +0100

    Add first demo projects

---
+++ Programming in C sharp/Hello World/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Hello_World
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
